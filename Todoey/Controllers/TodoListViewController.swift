238ecddc2a7b2ba148eb2ab581230f9cf11ecd3b
Fantomas <1234556789>
2021-11-29 21:14:12 +0200

    add Item.swift file

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Item 1", "Item 2", "Item 3"]
+    var itemArray = [Item]()
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Todoey"
+        
+        let newItem1 = Item()
+        newItem1.title = "Item 1"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Item 2"
+        itemArray.append(newItem2)
+
+        let newItem3 = Item()
+        newItem2.title = "Item 3"
+        itemArray.append(newItem3)
+        
         customizeAppearance()
         customizeRightBarButton()
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
     }
     
     //MARK: - Func
     
     fileprivate func customizeAppearance() {
+        title = "Todoey"
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = .systemBlue
@@ -42,7 +59,11 @@ class TodoListViewController: UITableViewController {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            self.defaults.set(newItem, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
@@ -62,7 +83,9 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
         
         return cell
     }
@@ -70,13 +93,9 @@ class TodoListViewController: UITableViewController {
     //MARK: - Tableview Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
     }
 
 }
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Onopriienko.Sergii on 26.11.2021.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool    = false
+}
